fa755172fbc7678504539fc17a9e065c35cc06f5
daniel <daniel@outboss.io>
2021-10-31 11:08:40 -0400

    add component DestinoViaje

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DestinoViajeComponent
   ],
   imports: [
     BrowserModule
